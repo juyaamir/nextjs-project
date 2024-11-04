f95a05d4fe7a206bedc1a98e5b64accf5b935302
Juya <juyaamirm@gmail.com>
2024-11-04 22:43:01 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import { Lusitana  } from 'next/font/google';
+import { Oleo_Script } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin'], weight: ['200'], style: 'italic' });
+
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['700'] });
+
+export const oleo_script = Oleo_Script({subsets: ['latin'], weight: '700' })
